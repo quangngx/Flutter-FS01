b384417f73bd1150399e0f9cbb5271375ef25473
Ngọc Quang <1812832@dlu.edu.vn>
2021-10-31 16:42:06 +0700

    add app contants

---
+++ social_network_app/lib/constants/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  //Number
+  static const double regularRadius = 24;
+  static const double shortRadius = 16;
+  static const double shorterRadius = 8;
+
+  static const double regularPadding = 16;
+  static const double dotSized = 18;
+  //String
+}
