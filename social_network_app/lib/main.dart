3226e8aa5c062949effa789d3726bfbde1558723
Quang Nguyễn <2jiro.jp@gmail.com>
2022-07-23 16:08:25 +0700

    fix bug firebase on ios
    [VERBOSE-2:ui_dart_state.cc(209)] Unhandled Exception: [core/not-initialized]

--- social_network_app/lib/main.dart
+++ social_network_app/lib/main.dart
@@ -1,11 +1,21 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isIOS) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   await Firebase.initializeApp();
   runApp(const App());
 }
